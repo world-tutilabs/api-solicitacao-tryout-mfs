b51768d5c3105a11452ef4c92d87da40afd13824
RafaelRailton <rafaelrailton@gmail.com>
2022-06-22 08:44:58 -0400

    chore: add docker-compose

--- src/infra/db/mysql/tryout-repository/add.ts
+++ src/infra/db/mysql/tryout-repository/add.ts
@@ -5,41 +5,43 @@ import {PrismaHelper} from '../helpers/prisma-helper';
 
 export class AddTryoutMysqlRepository implements AddTryoutRepository{
   async add (tryoutData: AddTryoutModel): Promise<TryoutModel>{ 
-      // const result = await PrismaHelper.prisma.solicitationTryout.create({
-      //       data: {
-      //         code_sap: tryoutData.code_sap,
-      //         desc_product: tryoutData.product_description,
-      //         client: tryoutData.client,
-      //         programmed_date: new Date(),
-      //         reason: tryoutData.reason,
-      //         injectionProcess: {
-      //           create: {
-      //             proc_technician: {tecnico: "Rafael Almeida"},
-      //             quantity: 10,
-      //             labor: {
-      //               create: {
-      //                 description: tryoutData.InjectionProcess.labor.description,
-      //                 amount: tryoutData.InjectionProcess.labor.amount
-      //               }
-      //             },
-      //             feedstocks: {
-      //               create: {
-      //                 description: tryoutData.InjectionProcess.feedstocks.description,
-      //                 code: tryoutData.InjectionProcess.feedstocks.code
-      //               }
-      //             },
-      //             molde: {
-      //               create: {
-      //                 desc_mold: tryoutData.InjectionProcess.mold.mold,
-      //                 number_cavity: tryoutData.InjectionProcess.mold.number_cavity
-      //               }
-      //             }
-      //           }
-      //         }
-      //       }
-      //     })
-          // console.log(result)
-          // const FindAllTryout = await PrismaHelper.prisma.tryout.findMany({
+    console.log('entrou')
+      const result = await PrismaHelper.prisma.solicitationTryout.create({
+            data: {
+              code_sap: tryoutData.code_sap,
+              desc_product: tryoutData.product_description,
+              client: tryoutData.client,
+              programmed_date: new Date(),
+              reason: tryoutData.reason,
+              injectionProcess: {
+                create: {
+                  proc_technician: "rafael almeida",
+                  quantity: 10,
+                  labor: {
+                    create: {
+                      description: tryoutData.InjectionProcess.labor.description,
+                      amount: tryoutData.InjectionProcess.labor.amount
+                    }
+                  },
+                  mold: {
+                    create: {
+                      desc_mold: tryoutData.InjectionProcess.mold.mold,
+                      number_cavity: tryoutData.InjectionProcess.mold.number_cavity
+                    }
+                  },
+                  feedstock: {
+                    create: {
+                      code: tryoutData.InjectionProcess.feedstocks.code,
+                      description: tryoutData.InjectionProcess.feedstocks.description
+                    }
+                  }
+                }
+              }
+            }
+          })
+          console.log(result)
+          console.log('entrou')
+          // const FindAllTryout = await PrismaHelper.prisma.solicitationTryout.findMany({
           //   where: { id: result.id },
           //   include: {
           //     injection_process: {
@@ -54,7 +56,6 @@ export class AddTryoutMysqlRepository implements AddTryoutRepository{
           //   }
           // })
           // const mapInjectionProcess = await PrismaHelper.mapInjectionProcess(FindAllTryout)
-    
         return null
     }
   
