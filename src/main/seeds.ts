bee029007d2e70c78e9bcac05d4b2d4f5973cca2
yan.batista <yantutilabs@gmail.com>
2023-08-11 15:11:49 -0300

    🌱 seed: seed homologation_status modificada e adicionada nova seed para a tabel "reason"

--- src/main/seeds.ts
+++ src/main/seeds.ts
@@ -1,25 +1,47 @@
 import { PrismaHelper } from "../infra/Database/mysql/helpers/prisma-helper";
-const seeds = async (): Promise<void> =>  {
- let isValid = await PrismaHelper.prisma.homologationStatus.count()
- if (isValid == 0) {
-
-  const data = [
-     { id: 1, description: 'Aprovado' },
-     { id: 2, description: 'Reprovado' },
-     { id: 3, description: 'Revisao'},
-     { id: 4, description: 'Cancelado'},
-     { id: 5, description: 'Concluido' }
-  ]
+const seeds = async (): Promise<void> => {
+  let homologation_status =
+    await PrismaHelper.prisma.homologationStatus.count();
+  const reason = await PrismaHelper.prisma.reason.count();
+  if (homologation_status === 0) {
+    const data = [
+      { id: 1, description: "Solicitação Aprovada" },
+      { id: 2, description: "Solicitação Reprovada" },
+      { id: 3, description: "Análise PCP" },
+      { id: 4, description: "Cancelado" },
+      { id: 5, description: "Relatório Aprovado" },
+      { id: 6, description: "Relatório Reprovado" },
+    ];
     const result = await PrismaHelper.prisma.homologationStatus.createMany({
-      data
-     })
-     
-   console.log(`Creation Seeds homologationStatus: ${ JSON.stringify(result)}`)
-  
-  return
- }
- console.log('Seeds already created!')
+      data,
+    });
 
-} 
+    console.log(
+      `Seeded successfully on table 'homologationStatus': ${JSON.stringify(
+        result
+      )}`
+    );
 
-seeds()
+    if (reason === 0) {
+      const data = [
+        { id: 1, description: "Novo" },
+        { id: 2, description: "Retroativo" },
+        { id: 3, description: "Novo Produto" },
+        { id: 4, description: "Modificação de Molde" },
+      ];
+      const result = await PrismaHelper.prisma.reason.createMany({
+        data,
+      });
+
+      console.log(
+        `Seeded successfully on table 'reason': ${JSON.stringify(result)}`
+      );
+    }
+
+    return;
+  }
+
+  console.log("Seeds already created!");
+};
+
+seeds();
